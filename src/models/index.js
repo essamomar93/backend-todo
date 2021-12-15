409731903e0452ebf30f3e33e6349e820e578dce
essam omar <essam.93@hotmail.com>
2021-12-15 19:20:26 +0200

    2

--- src/models/index.js
+++ src/models/index.js
@@ -7,10 +7,15 @@ const Collection = require('../models/data-collection');
 const { Sequelize, DataTypes } = require('sequelize');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory;';
-
-const sequelize = new Sequelize(DATABASE_URL);
-
-
+let sequelizeOptions = process.env.NODE_ENV === 'production' ? {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  }
+} : {};
+const sequelize = new Sequelize(DATABASE_URL,sequelizeOptions);
 const items = itemsModel(sequelize, DataTypes);
 
 module.exports = {
